a097563134030c0b7720d95ef9320fe3470beb31
Abhinav Anand <abhinav.anand101@gmail.com>
2022-12-31 11:59:01 +0530

    git

--- LinkedList/LinkListTraversal.java
+++ LinkedList/LinkListTraversal.java
@@ -3,7 +3,7 @@ package LinkedList;
 public class LinkListTraversal {
 
     public  void printNode(Node head){
-        
+        //add
         Node current =head;
         while(current!=null){
             System.out.print(current.data+" ");
